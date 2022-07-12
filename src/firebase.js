b30125bf28c554df55b48d98ffad0e349b90c276
Anubha13kumari <anubhamuz@gmail.com>
2022-07-12 11:25:08 +0530

    added environment variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCy_qXwe7tlrtxQg-DdAaYzsxPoPN_PDUQ",
-  authDomain: "instagram-clone-react-aa2aa.firebaseapp.com",
-  projectId: "instagram-clone-react-aa2aa",
-  storageBucket: "instagram-clone-react-aa2aa.appspot.com",
-  messagingSenderId: "7827069926",
-  appId: "1:7827069926:web:829458f8db5640decbd427",
-  measurementId: "G-HPPKDB7YR6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APIID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 });
-
-
 
 const db=firebaseApp.firestore();
 const auth=firebase.auth();
